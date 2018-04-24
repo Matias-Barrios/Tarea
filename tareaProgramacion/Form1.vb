7314b900adae5defcccbf63a462313f0056aa9b5
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-04-24 13:48:36 -0500

    mas

--- tareaProgramacion/Form1.vb
+++ tareaProgramacion/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub vacio(sender As Object, e As EventArgs) Handles txtNombre.TextChanged, txtApellido.TextChanged
-        Panel1.Enabled = (txtNombre.Text <> "" And txtApellido.Text <> "")
+        Panel1.Enabled = (txtNombre.Text <> "" AndAlso txtApellido.Text <> "")
     End Sub
 End Class
